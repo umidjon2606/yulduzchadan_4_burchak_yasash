10f50984030c98b7f615c0ce6798c91b3305469c
umidjon2606 <110013094+umidjon2606@users.noreply.github.com>
2022-07-30 02:40:29 -0400

    Add files via upload

---
+++ untitled53/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // write your code here
+        int n = 10;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+                System.out.print("****");
+            }
+
+            System.out.println();
+        }
+    }
+}
